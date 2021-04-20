3b7f97759af7295cd17f5e0fbfa6d930163e4a50
Trevor <trevorj342@gmail.com>
2021-04-20 10:26:03 -0400

    Remove GlobalStorage (moved into form)

--- bHapticsIntegration.Installer/Forms/IEYTDInstall.cs
+++ bHapticsIntegration.Installer/Forms/IEYTDInstall.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.IO;
+using System.Net;
+using System.Diagnostics;
 using System.Windows.Forms;
 
-using bHapticsIntegration.Installer.Classes;
+using EpicMorg.SteamPathsLib;
+using EpicMorg.SteamPathsLib.Model;
 
 namespace bHapticsIntegration.Installer.Forms
 {
     public partial class IEYTDInstall : Form
     {
-        public IEYTDInstall()
-        {
-            InitializeComponent();
-        }
+        public string ieytdGamePath;
+        public const int ieytdSteamAppId = 587430;
+        public const string baseUrl = "https://trevtv.github.io/ieytd";
+
+        public WebClient webClient = new WebClient();
+
+        public IEYTDInstall() => InitializeComponent();
 
         private void FormLoad(object sender, EventArgs e)
         {
@@ -19,17 +25,30 @@ namespace bHapticsIntegration.Installer.Forms
             FolderTextBox.Enabled = false;
             ContinueButton.Enabled = false;
 
-            new GlobalStorage();
+            FindIEYTDDirectory();
             TopMainText.Visible = false;
 
-            if (string.IsNullOrEmpty(GlobalStorage.Instance.ieytdGamePath))
-            {
+            if (string.IsNullOrEmpty(ieytdGamePath))
                 MessageBox.Show("Unable to find install location, please select manually.", "");
-            }
-            else FolderTextBox.Text = GlobalStorage.Instance.ieytdGamePath;
+            else FolderTextBox.Text = ieytdGamePath;
 
             SelectFolderButton.Enabled = true;
             ContinueButton.Enabled = true;
+        }
+
+        private void FindIEYTDDirectory()
+        {
+            SteamAppRegistryData registry = SteamPathsUtil.GetSteamAppDataById(ieytdSteamAppId);
+            SteamAppManifestData manifest = SteamPathsUtil.GetSteamAppManifestDataById(ieytdSteamAppId);
+
+            bool installed = registry?.Installed ?? false;
+
+            Debug.WriteLine("Installed: " + installed);
+            if (installed)
+            {
+                Debug.WriteLine("Install Directory: " + manifest?.Path);
+                ieytdGamePath = manifest?.Path;
+            }
         }
 
         private void SelectIEYTDFolder(object sender, EventArgs e)
@@ -37,13 +56,12 @@ namespace bHapticsIntegration.Installer.Forms
             DialogResult result = FileDialog.ShowDialog();
 
             if (result == DialogResult.OK)
-            {
                 FolderTextBox.Text = Path.GetDirectoryName(FileDialog.FileName);
-            }
         }
 
         private void Install(object sender, EventArgs e)
         {
+            string currentRelease = "";
         }
     }
 }
